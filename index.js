0a805027fe072c93f03f17fb31e7e9780ff6bbc2
Anthony Bui <anthonyb50@gmail.com>
2023-04-14 00:24:27 -0400

    Set up basic file structure for 2 databases

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const path = require('path');
 const port = 3001;
 
 const App = express();
+express.json();
+
+App.get('/', (req, res) => {
+  res.send('sup');
+});
 
 App.listen(port, () => {
   console.log(`Listening to port: ${port}`);
-})
+});
