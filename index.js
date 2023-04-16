1b251f911de71c3e88b9d073b1c7d1938e30d698
Anthony Bui <anthonyb50@gmail.com>
2023-04-16 11:57:33 -0400

    Added routing paths

--- index.js
+++ index.js
@@ -9,7 +9,29 @@ App.get('/', (req, res) => {
   res.send('sup');
 });
 
-App.get('/questions')
+//List Questions - Params: product_id, page, count
+App.get('/qa/questions')
+
+//Answer List - Params: product_id  Query param: page, count
+App.get('/qa/questions/:question_id/answers')
+
+//Add Question - Body params: body, name, email, product_id
+App.post('/qa/questions')
+
+//Add Answer - Params: question_id  Body params: body, name, email, photos
+App.post('/qa/questions/:question_id/answers')
+
+//Mark Question Helpful - Params: question_id
+App.put('/qa/questions/:question_id/helpful')
+
+//Report Question - Params: question_id
+App.put('/qa/questions/:question_id/report')
+
+//Mark Answer Helpful - Params: answer_id
+App.put('/qa/answers/:answer_id/helpful')
+
+//Report Answer - Params: answer_id
+App.put('/qa/answers/:answer_id/report')
 
 App.listen(port, () => {
   console.log(`Listening to port: ${port}`);
