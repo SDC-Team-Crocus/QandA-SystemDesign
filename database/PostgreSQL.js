32bdf9f15a9b2b1e1d3e62f15e0e087f5ffecc4d
Anthony Bui <anthonyb50@gmail.com>
2023-04-22 16:23:57 -0400

    Completed markhelpful and report functions

--- database/PostgreSQL.js
+++ database/PostgreSQL.js
@@ -11,16 +11,19 @@ const login = {
 const pool = new Pool(login);
 pool.connect();
 
+//Adds new asker or answerer user
 async function insertUser (name, email) {
   const inserted = await pool.query("INSERT INTO Users (UserName, Email) VALUES ($1, $2) ON CONFLICT DO NOTHING", [name, email]);
 };
 
+//Iterate through photos array and posts to database
 async function insertPhotos (answerid, photos) {
   for (let i = 0; i < photos.length; i++) {
     await pool.query("INSERT INTO Photos (AnswerID, PhotoURL) VALUES ($1, $2)", [answerid, photos[i]]);
   }
 }
 
+//Get photos. 1 format for answers and another for questions
 async function getPhotos (answerID, method) {
   const photosData = await pool.query("SELECT * FROM Photos WHERE AnswerID = $1", [answerID])
   let photosContainer = [];
@@ -39,6 +42,7 @@ async function getPhotos (answerID, method) {
   return photosContainer;
 };
 
+//Gets answers for question id. 2 formats. 1 for when questions format is called and 1 for answer api format. Calls photos
 async function getAnswers (questionID, method, count, page) {
   if (!count) {
     count = 100;
@@ -80,6 +84,7 @@ async function getAnswers (questionID, method, count, page) {
   }
 };
 
+//Gets all questions for product ids, calls for answers and photos as well
 async function getQuestions (productID, count, page, method) {
   if (!count) {
     count = 5;
@@ -105,15 +110,35 @@ async function getQuestions (productID, count, page, method) {
   return questionsContainer;
 };
 
+//Posts question to database
 async function postQuestion (body, name, email, productID) {
   await insertUser(name, email);
   return pool.query("INSERT INTO Questions (QuestionBody, UserID, ProductID, CurrentDate) VALUES ($1, (SELECT UserID FROM Users WHERE UserName = $2), $3, $4)", [body, name, productID, (new Date()).getTime()]);
 }
 
+//Posts answer to database
 async function postAnswer (body, name, email, questionID, photos) {
   await insertUser(name, email);
   return pool.query("INSERT INTO Answers (AnswerBody, UserID, QuestionID, CurrentDate) VALUES ($1, (SELECT UserID FROM Users WHERE UserName = $2), $3, $4) RETURNING AnswerID", [body, name, questionID, (new Date()).getTime()])
   .then(data => (insertPhotos(data.rows[0].answerid, photos)))
+}
+
+async function markHelpful (id, method) {
+  if (method === "question") {
+    return pool.query("UPDATE Questions SET Helpfulness = Helpfulness + 1 WHERE QuestionID = $1", [id])
+  }
+  if (method === "answer") {
+    return pool.query("UPDATE Answers SET Helpfulness = Helpfulness + 1 WHERE AnswerID = $1", [id])
+  }
+}
+
+async function report (id, method) {
+  if (method === "question") {
+    return pool.query("UPDATE Questions SET Reported = 1 WHERE QuestionID = $1", [id])
+  }
+  if (method === "answer") {
+    return pool.query("UPDATE Answers SET Reported = 1 WHERE AnswerID = $1", [id])
+  }
 }
 
 module.exports.getQuestions = getQuestions;
