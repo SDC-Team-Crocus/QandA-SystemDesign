cbf15cd5986d6e1ca8eaeda7178f198ed239628b
Anthony Bui <anthonyb50@gmail.com>
2023-05-02 21:46:22 -0400

    Initializing test suites for request methods

--- test.js
+++ test.js
@@ -1,5 +1,26 @@
+describe('Unit Tests for all GET Requests', () => {
 
-describe('This is the test suite, hellooooo', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect((1 + 2)).toBe(3);
+  });
+
+  test('Uh oh, failing test', () => {
+    expect(false).toBe(true);
+  })
+})
+
+describe('Unit Tests for all POST Requests', () => {
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect((1 + 2)).toBe(3);
+  });
+
+  test('Uh oh, failing test', () => {
+    expect(false).toBe(true);
+  })
+})
+
+describe('Unit Tests for all PUT Requests', () => {
 
   test('adds 1 + 2 to equal 3', () => {
     expect((1 + 2)).toBe(3);
