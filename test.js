6bf561302e3e893ddb18d9bf26f4316d93ae6c11
Anthony Bui <anthonyb50@gmail.com>
2023-05-02 21:59:10 -0400

    Set up each test title

--- test.js
+++ test.js
@@ -1,32 +1,45 @@
 describe('Unit Tests for all GET Requests', () => {
 
-  test('adds 1 + 2 to equal 3', () => {
+  test('It should get all QUESTIONS for a specific Product ID', () => {
     expect((1 + 2)).toBe(3);
   });
 
-  test('Uh oh, failing test', () => {
+  test('It should get all ANSWERS for a specific Question ID', () => {
     expect(false).toBe(true);
-  })
+  });
 })
 
 describe('Unit Tests for all POST Requests', () => {
 
-  test('adds 1 + 2 to equal 3', () => {
+  test('It should add a QUESTION to the database tables based on Product ID', () => {
     expect((1 + 2)).toBe(3);
   });
 
-  test('Uh oh, failing test', () => {
+  test('It should add an ANSWER with IMAGES to the database tables based on Question ID', () => {
     expect(false).toBe(true);
-  })
+  });
+
+  test('It should add an ANSWER without IMAGES to the database tables based on Question ID', () => {
+    expect(false).toBe(true);
+  });
+
 })
 
 describe('Unit Tests for all PUT Requests', () => {
 
-  test('adds 1 + 2 to equal 3', () => {
+  test('It should increase a QUESTION\'s helpfulness by 1', () => {
     expect((1 + 2)).toBe(3);
   });
 
-  test('Uh oh, failing test', () => {
+  test('It should increase an ANSWER\'s helpfulness by 1', () => {
     expect(false).toBe(true);
-  })
+  });
+
+  test('It should change QUESTIONS\'s report status to true and stay as true', () => {
+    expect(false).toBe(true);
+  });
+
+  test('It should change ANSWER\'s report status to true and stay as true', () => {
+    expect(false).toBe(true);
+  });
 })
