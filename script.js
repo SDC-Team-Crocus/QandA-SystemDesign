7ade8bafd59ee50ae99806f2b276f39ae0878b70
Anthony Bui <anthonyb50@gmail.com>
2023-05-11 21:43:06 -0400

    K6 stress testing

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
   //Virtual Users
-  vus: 10,
+  vus: 5000,
   //Duration
-  duration: '30s',
+  duration: '1m',
 };
 export default function () {
-  http.get('http://localhost:3001/qa/questions/1111/answers');
-  // http.get('http://localhost:3001/qa/questions?product_id=71701');
+  // http.get('http://localhost:3001/qa/questions/1111/answers');
+  http.get('http://localhost:3001/qa/questions?product_id=71705');
   sleep(1);
 }
