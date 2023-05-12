43274343f1e637669e66159ff8d8a1e0964b15d6
Anthony Bui <anthonyb50@gmail.com>
2023-05-11 21:50:40 -0400

    Updated K6 test

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+// export const options = {
+//   //Virtual Users
+//   vus: 5000,
+//   //Duration
+//   duration: '1m',
+// };
 export const options = {
-  //Virtual Users
-  vus: 5000,
-  //Duration
-  duration: '1m',
-};
+  discardResponseBodies: true,
+  scenarios: {
+    contacts: {
+      //Method of rate
+      executor: 'constant-arrival-rate',
+      //How long test runs
+      duration: '1m',
+      //Rate per timeUnit
+      rate: 1000,
+      //Time Unit
+      timeUnit: '1s',
+      //Initial Virtual Users
+      preAllocatedVUs: 2,
+      //Max Virtual Users
+      maxVUs: 1100
+    }
+  }
+}
 export default function () {
   // http.get('http://localhost:3001/qa/questions/1111/answers');
   http.get('http://localhost:3001/qa/questions?product_id=71705');
